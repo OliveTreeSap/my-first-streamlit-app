177653f15e7bebb76867bc2f6efdfc97e3d8f92c
OliveTreeSap <111682362+OliveTreeSap@users.noreply.github.com>
2023-03-17 21:36:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ model = pickle.load(open('model.pickle', 'rb'))
 st.title("Revenue Prediction")
 temp = st.number_input('Input Temperature')
 if st.button("Predict"):
-    revenue = model.predict(temp.reshape(-1, 1)
+    revenue = model.predict(temp.reshape(-1, 1))
     st.success(f"{revenue}")
