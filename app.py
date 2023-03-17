8b3c9934e8fd5b1cc8fab6005d7b6e09ada2c686
OliveTreeSap <111682362+OliveTreeSap@users.noreply.github.com>
2023-03-17 21:22:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import streamlit as st
 import sklearn
 import plotly
 
-st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
-a = st.number_input('Tham số a')
-b = st.number_input('Tham số b')
-if st.button("Giải"):
+st.title("Revenue Prediction")
+temp = st.number_input('Input Temperature')
+if st.button("Predict"):
     x = (-b)/a
     st.success(f"Phương trình có một nghiệm {x}")
