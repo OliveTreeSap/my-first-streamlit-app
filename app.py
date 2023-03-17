81954d2e871ff598ad5455dac59ddc2b2de6a319
OliveTreeSap <111682362+OliveTreeSap@users.noreply.github.com>
2023-03-17 21:28:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import sklearn
 import pickle
+import numpy as np
 model = pickle.load(open('model.pickle', 'rb'))
 
 st.title("Revenue Prediction")
 temp = st.number_input('Input Temperature')
 if st.button("Predict"):
-    revenue = model.predict(temp)
+    revenue = model.predict(temp.reshape(-1, 1)
     st.success(revenue)
